1d3d082dae7ac2df356d26e5129af2afbded6c45
yogitaranwa <yogita2004ranwa@gmail.com>
2026-01-22 14:55:34 +0530

    feat: add Vercel Analytics integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SmoothScroll from "@/components/SmoothScroll"; // Imports your updated component
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,8 @@ export default function RootLayout({
         <SmoothScroll> 
           {children}
         </SmoothScroll> 
+
+        <Analytics />
       </body>
     </html>
   );
